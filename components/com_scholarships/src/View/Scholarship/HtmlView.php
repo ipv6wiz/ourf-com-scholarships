9b132f04c16cdc2551ea6bdd26ddaf89c52579ee
john <john@thejohnpage.com>
2021-11-27 22:01:54 -0800

    Added first frontend display file

---
+++ components/com_scholarships/src/View/Scholarship/HtmlView.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     ${NAMESPACE}
+ * @subpackage  com_scholarships
+ *
+ * @copyright   Copyright (C) 2006 - 2021 MultiMediaCommunications. All rights reserved.
+ * @license     GPL2
+ */
+
+namespace OURF\Component\Scholarships\Site\View\Foo;
+\defined('_JEXEC') or die;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+/**
+ * HTML Scholarships View class for the Foo component
+ *
+ * @since  __BUMP_VERSION__
+ */
+class HtmlView extends BaseHtmlView
+{
+    /**
+     * Execute and display a template script.
+     *
+     * @param string $tpl The name of the template file to parse; automatically searches through the template paths.
+     *
+     * @return  mixed  A string if successful, otherwise an Error object.
+     *
+     * @throws \Exception
+     * @since sometime
+     */
+    public function display($tpl = null): mixed
+    {
+        parent::display($tpl);
+        return true;
+    }
+}
---
+++ components/com_scholarships/tmpl/scholarship/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     ${NAMESPACE}
+ * @subpackage  com_scholarships
+ *
+ * @copyright   Copyright (C) 2006 - 2021 MultiMediaCommunications. All rights reserved.
+ * @license     GPL2
+ */
+
+\defined('_JEXEC') or die;
+?>
+Hello Scholarships Frontend
