d8585cbb3919f26bc26920041b51d47e06ba92ef
john <john@thejohnpage.com>
2021-12-27 13:28:02 -0800

    Added ScholarshipsController - bug fixes - v1.0.23

---
+++ administrator/components/com_scholarships/src/Controller/ScholarshipsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package     ${NAMESPACE}
+ * @subpackage  com_scholarships
+ *
+ * @copyright   Copyright (C) 2006 - 2021 MultiMediaCommunications. All rights reserved.
+ * @license     GPL2
+ */
+
+namespace OURF\Component\Scholarships\Administrator\Controller;
+\defined('_JEXEC') or die;
+use Joomla\CMS\MVC\Controller\FormController;
+
+class ScholarshipsController extends FormController
+{
+
+}
