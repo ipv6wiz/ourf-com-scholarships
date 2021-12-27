7760958043ec47675a2b2ad16a8099eca5c95b89
john <john@thejohnpage.com>
2021-12-27 13:32:30 -0800

    Added ScholarshipsController - bug fixes - v1.0.24

--- administrator/components/com_scholarships/src/Controller/ScholarshipsController.php
+++ administrator/components/com_scholarships/src/Controller/ScholarshipsController.php
@@ -9,9 +9,14 @@
 
 namespace OURF\Component\Scholarships\Administrator\Controller;
 \defined('_JEXEC') or die;
-use Joomla\CMS\MVC\Controller\FormController;
 
-class ScholarshipsController extends FormController
+use Joomla\CMS\MVC\Controller\AdminController;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+
+class ScholarshipsController extends AdminController
 {
-
+    public function __construct($config = array(), MVCFactoryInterface $factory = null, $app = null, $input = null)
+    {
+        parent::__construct($config, $factory, $app, $input);
+    }
 }
