181c064fe4296d4ec6e91e70d82cfde2efbd4f11
john <john@thejohnpage.com>
2022-01-18 14:52:24 -0800

    Added search toolbar - modified  list select - v1.0.70

--- administrator/components/com_scholarships/src/Model/ScholarshipsModel.php
+++ administrator/components/com_scholarships/src/Model/ScholarshipsModel.php
@@ -10,6 +10,7 @@
 namespace OURF\Component\Scholarships\Administrator\Model;
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Associations;
 use Joomla\CMS\MVC\Model\ListModel;
@@ -40,11 +41,141 @@ class ScholarshipsModel extends ListModel
         parent::__construct($config);
     }
 
+    /**
+     * Get the filter form
+     *
+     * @param   array    $data      data
+     * @param   boolean  $loadData  load current data
+     *
+     * @return  \Joomla\CMS\Form\Form  The \JForm object or null if the form can't be found
+     *
+     * @since   3.2
+     */
+    public function getFilterForm($data = array(), $loadData = true)
+    {
+        $form = parent::getFilterForm($data, $loadData);
+
+        $params = ComponentHelper::getParams('com_scholarships');
+
+        if (!$params->get('workflow_enabled'))
+        {
+            $form->removeField('stage', 'filter');
+        }
+        else
+        {
+            $ordering = $form->getField('fullordering', 'list');
+
+            $ordering->addOption('JSTAGE_ASC', ['value' => 'ws.title ASC']);
+            $ordering->addOption('JSTAGE_DESC', ['value' => 'ws.title DESC']);
+        }
+
+        return $form;
+    }
+
+    /**
+     * Method to auto-populate the model state.
+     *
+     * Note. Calling getState in this method will result in recursion.
+     *
+     * @param   string  $ordering   An optional ordering field.
+     * @param   string  $direction  An optional direction (asc|desc).
+     *
+     * @return  void
+     *
+     * @since   1.6
+     */
+    protected function populateState($ordering = 'a.year', $direction = 'desc')
+    {
+        $app = Factory::getApplication();
+
+        $forcedLanguage = $app->input->get('forcedLanguage', '', 'cmd');
+
+        // Adjust the context to support modal layouts.
+        if ($layout = $app->input->get('layout'))
+        {
+            $this->context .= '.' . $layout;
+        }
+
+        // Adjust the context to support forced languages.
+        if ($forcedLanguage)
+        {
+            $this->context .= '.' . $forcedLanguage;
+        }
+
+        $search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+        $this->setState('filter.search', $search);
+
+        $featured = $this->getUserStateFromRequest($this->context . '.filter.featured', 'filter_featured', '');
+        $this->setState('filter.featured', $featured);
+
+        $published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
+        $this->setState('filter.published', $published);
+
+        $language = $this->getUserStateFromRequest($this->context . '.filter.language', 'filter_language', '');
+        $this->setState('filter.language', $language);
+
+        $formSubmited = $app->input->post->get('form_submited');
+
+        // Gets the value of a user state variable and sets it in the session
+        $this->getUserStateFromRequest($this->context . '.filter.scholarship_recipient', 'filter_scholarship_recipient');
+        $this->getUserStateFromRequest($this->context . '.filter.scholarship_college_name', 'filter_scholarship_college_name');
+        $this->getUserStateFromRequest($this->context . '.filter.scholarship_department_name', 'filter_scholarship_department_name', '');
+
+        if ($formSubmited)
+        {
+            $authorId = $app->input->post->get('scholarship_recipient');
+            $this->setState('filter.scholarship_recipient', $authorId);
+
+            $categoryId = $app->input->post->get('scholarship_college_name');
+            $this->setState('filter.scholarship_department_name', $categoryId);
+
+        }
+
+        // List state information.
+        parent::populateState($ordering, $direction);
+
+        // Force a language
+        if (!empty($forcedLanguage))
+        {
+            $this->setState('filter.language', $forcedLanguage);
+            $this->setState('filter.forcedLanguage', $forcedLanguage);
+        }
+    }
+
     protected function getListQuery()
     {
         $db = $this->getDbo();
         $query = $db->getQuery(true);
-        $query->select([
+        $user  = Factory::getUser();
+
+        $params = ComponentHelper::getParams('com_scholarships');
+
+        $query->select(
+            $this->getState(
+                'list.select',
+                [
+                    $db->quoteName(),
+                    $db->quoteName('a.id'),
+                    $db->quoteName('a.state'),
+                    $db->quoteName('a.version'),
+                    $db->quoteName('a.ordering'),
+                    $db->quoteName('a.created'),
+                    $db->quoteName('a.created_by'),
+                    $db->quoteName('a.created_by_alias'),
+                    $db->quoteName('a.modified'),
+                    $db->quoteName('a.featured'),
+                    $db->quoteName('a.language'),
+                    $db->quoteName('a.catid'),
+                    $db->quoteName('a.alias'),
+                    $db->quoteName('a.language'),
+                    $db->quoteName('scholarship_topic'),
+                    $db->quoteName('scholarship_employment'),
+                    $db->quoteName('scholarship_abstract_title')
+                ]
+            )
+        )
+
+        ->select([
             $db->quoteName('a.id', 'id'),
             $db->quoteName('scholarship_year', 'year'),
             $db->quoteName('scholarship_recipient', 'recipient'),
@@ -57,6 +188,8 @@ class ScholarshipsModel extends ListModel
             ->join('LEFT', $db->quoteName('#__scholarship_status', 'b'), $db->quoteName('scholarship_fk_scholarship_status').' = '.$db->quoteName('b.id'))
             ->join('LEFT', $db->quoteName('#__scholarship_colleges', 'c'), $db->quoteName('scholarship_fk_scholarship_college').' = '.$db->quoteName('c.id'))
             ->join('LEFT', $db->quoteName('#__scholarship_departments', 'd'), $db->quoteName('scholarship_fk_scholarship_department').' = '.$db->quoteName('d.id'))
+            ->join('LEFT', $db->quoteName('#__languages', 'l'), $db->quoteName('l.lang_code') . ' = ' . $db->quoteName('a.language'))
+            ->join('LEFT', $db->quoteName('#__users', 'ua'), $db->quoteName('ua.id') . ' = ' . $db->quoteName('a.created_by'))
             ->join('LEFT', $db->quoteName('#__categories', 'e'), $db->quoteName('e.id').' =  '.$db->quoteName('a.catid'));
         return $query;
     }
